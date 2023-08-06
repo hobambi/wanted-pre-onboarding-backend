99f902ab235b339956cb05f9fda3214be8d2a296
hobambi <yyunsunny@gmail.com>
2023-08-06 12:54:05 +0900

    initial commit

---
+++ src/main/java/com/example/wantedbackendinternship/WantedBackendInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.wantedbackendinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedBackendInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedBackendInternshipApplication.class, args);
+	}
+
+}
